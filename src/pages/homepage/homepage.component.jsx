2d60db9836bdfd20b13ad9ad02bc63ed44123d87
elisee dianda <eliseedianda.end@gmail.com>
2021-11-02 15:03:47 +0200

    update navigation

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import "./homepage.style.scss";
 import Directory from "../../components/directory/directory.component";
 
-const Homepage = () => {
+const Homepage = ({ history }) => {
   return (
     <div className="homepage">
-      <Directory></Directory>
+      <Directory history={history}></Directory>
     </div>
   );
 };
