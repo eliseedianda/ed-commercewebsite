4b790c7e85b981a403e8f2568d5996435e108044
elisee dianda <eliseedianda.end@gmail.com>
2021-11-02 12:34:02 +0200

    finish with first part styling

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import MenuItem from "../menu-item/menu-item.component";
+
+import "./directory.style.scss";
+
+class Directory extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      sections: [
+        {
+          title: "hats",
+          imageUrl:
+            "https://images.unsplash.com/photo-1572460737616-866a25d4d61a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80", //"https://i.ibb.co/cvpntL1////hats.png",
+          id: 1,
+          linkUrl: "shop/hats",
+        },
+        {
+          title: "jackets",
+          imageUrl:
+            "https://images.unsplash.com/photo-1559551409-dadc959f76b8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=873&q=80",
+
+          id: 2,
+          linkUrl: "shop/jackets",
+        },
+        {
+          title: "sneakers",
+          imageUrl:
+            "https://images.unsplash.com/photo-1612902376491-7a8a99b424e8?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8c25lYWtlcnN8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60",
+
+          id: 3,
+          linkUrl: "shop/sneakers",
+        },
+        {
+          title: "womens",
+          imageUrl:
+            "https://media.istockphoto.com/photos/know-ill-find-something-i-like-here-picture-id694044976?b=1&k=20&m=694044976&s=170667a&w=0&h=hyFGMdc5zYe1CS8BKdsfjpKjDaBVUqj-14JApZpZJSk=",
+
+          size: "large",
+          id: 4,
+          linkUrl: "shop/womens",
+        },
+        {
+          title: "mens",
+          imageUrl:
+            "https://images.unsplash.com/photo-1555529771-835f59fc5efe?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bWVucyUyMGNsb3RoaW5nfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60",
+
+          size: "large",
+          id: 5,
+          linkUrl: "shop/mens",
+        },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div className="directory-menu">
+        {this.state.sections.map(({ title, imageUrl, id, size }) => (
+          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Directory;
