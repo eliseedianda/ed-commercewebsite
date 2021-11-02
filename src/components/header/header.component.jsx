574e3a2a85ff33f19f48286baa17a82652591b89
elisee dianda <eliseedianda.end@gmail.com>
2021-11-02 19:38:15 +0200

    adding hearder component

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./header.style.scss";
+const Hearder = () => {
+  return (
+    <div className="header">
+      <Link className="logo-container" to="/">
+        <h2 className="logo">EDCOMMERCE</h2>
+      </Link>
+      <div className="options">
+        <Link className="option" to="/shop">
+          SHOP
+        </Link>
+        <Link className="option" to="/shop">
+          CONTACT
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Hearder;
